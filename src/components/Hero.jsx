be56b846c64b292b3599246dea72eb084b09a46a
My Name <hishamrehab309@gmail.com>
2024-08-02 15:55:26 +0300

    install gsap for animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
 
 const Hero = () => {
+    useGSAP(() => {
+        gsap.to()
+    }, [])
+
     return (
-        <div>Hero</div>
+        <section className='w-full nav-height bg-black relative'>
+            <div className='h-5/6 w-full flex-col'>
+                <p className='hero-title'>iPhone 15 Pro</p>
+            </div>
+        </section>
     )
 }
 
